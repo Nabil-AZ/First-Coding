406d95e7751fe88b65b63a201ed8b2b736e4d993
Nabil-AZ <nabilazraf06@gmail.com>
2026-09-07 16:56:29 +0700

    Add files via upload
    this is about variable

---
+++ coding nabil1.py
@@ -0,0 +1,49 @@
+#codingan pertama nabil 
+#mencoba python
+#variable
+#07-09=2026
+print("hello world")
+print()
+
+#String (berisi teks)
+nama = "Nabil"
+makanan_kesukaan = "masakan mama"
+kuliah = "MIT"
+
+print(f"nama saya {nama}")
+print(f"lalu makanan kesukaan saya adalah {makanan_kesukaan}")
+print(f"Saya sekarang sedang berkuliah di {kuliah}")
+print()
+
+#interger (berisi bilangan bulat)
+Umur = 17
+Sks_di_semester_awal = 20
+target_ipk = 4
+
+print(f"saya berumur {Umur} tahun")
+print(f"Sks pada awal semester saya sebanyak {Sks_di_semester_awal}")
+print(f"target ipk saya tiap semester adalah {target_ipk}")
+print()
+
+#float (berisi angka desimal)
+
+Ipk_saya = 4.0
+harga_barang = 10.525
+
+print(f"Tiap semester saya {Ipk_saya}")
+print(f"harga buku ini ${harga_barang}")
+print()
+
+#Boolean (berisi data true/false) DAN HARUS DENGAN BAHASA INGGRIS
+
+is_mahasiswa = False #bisa diganti True/False
+if is_mahasiswa:
+    print("Nabil is mahasiswa")
+else:
+    print("Nabil is not a mahasiswa")
+
+this_for_sale = True
+if this_for_sale:
+    print("this item is for sale")
+else:
+    print("this item is not for sale")
